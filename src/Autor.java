b8b6af60abfb41e2c7c99e2b4f330dc19df2db8c
AlexDv <DvoinikovAV@yandex.ru>
2024-02-05 20:02:29 +0300

    0

---
+++ src/Autor.java
@@ -0,0 +1,17 @@
+public class Autor{
+    private String fam;
+    private String ima;
+
+    public Autor(String fam,String ima){
+        this.fam=fam;
+        this.ima=ima;
+    }
+    public String getFam() {
+        return fam;
+    }
+    public String getIma() {
+        return ima;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Autor autor1=new Autor("Чехов","Антон");
+        Autor autor2=new Autor("Иоганн","Гетте");
+        Book book1=new Book("Фауст",autor2,2015);
+        Book book2=new Book("Вишневый сад",autor1,2014);
+        book1.setYearOfPublic(2016);
     }
 }
